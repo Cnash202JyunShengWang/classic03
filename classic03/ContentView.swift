53067e62e64070478a9837161054627f7b6726db
Roy <a0966750212@icloud.c0m>
2021-09-14 20:40:21 +0800

    mee9 14

--- classic03/ContentView.swift
+++ classic03/ContentView.swift
@@ -13,36 +13,110 @@ struct ContentView: View {
         TabView{
             ZStack{
             RoundedRectangle(cornerRadius: 30)
-                .fill(Color(#colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)))
-                .padding()
-            Text("我知道成功絕對不簡單")
-                
-                .font(.title)
-                .frame(width:300, height:50, alignment: .top)
+                .fill(Color(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)))
+                .ignoresSafeArea()
+                VStack{
+            Text("For me, here is the world's most lovely, but also the most sad a piece of land")
+                .italic()
+                .frame(width:300, height:200, alignment: .top)
                 .padding(.top,-200)
                 .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
                 .font(.subheadline)
-            Image(systemName: "sun.min")
+                .lineSpacing(5)
+                Text("對我來說，這裡是世界上最可愛，卻也是最悲傷的一片土地。")
+                    .frame(width:290, height:200, alignment: .top)
+                    .padding(.top,-150)
+                    .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                    .font(.subheadline)
+                    .lineSpacing(5)
+                }
+            Image("01")
                 .resizable()
-                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .top)
-                .padding(.top,-150)
+                .frame(width: 300, height:300, alignment: .center)
+                .cornerRadius(150)
+                .offset(x: 0, y: 100)
+                
             }
+            .tabItem { Image(systemName:"sun.min") }
             ZStack{
             RoundedRectangle(cornerRadius: 30)
-                .fill(Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)))
-                .padding()
+                .fill(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+                .ignoresSafeArea()
+                VStack{
+            Text("If you want to tame a person, will risk tears.")
+                .italic()
+                .frame(width:300, height:200, alignment: .top)
+                .padding(.top,-200)
+                .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                .font(.subheadline)
+                .lineSpacing(5)
+                Text("如果你要馴服一個人，就要冒著掉眼淚的危險。。")
+                    .frame(width:300, height:200, alignment: .top)
+                    .padding(.top,-150)
+                    .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                    .font(.subheadline)
+                    .lineSpacing(5)
+                }
+
+            Image("02")
+                .resizable()
+                .frame(width: 320, height:300, alignment: .center)
+                .offset(x: 0, y: 100)
             }
+            .tabItem { Image(systemName:"sun.min") }
             ZStack{
             RoundedRectangle(cornerRadius: 30)
-                    .fill(Color(#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)))
-                    .padding()
-            
-            
+                    .fill(Color(#colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)))
+                    .ignoresSafeArea()
+                VStack{
+            Text("I've always wanted to share his story, but always failed, can't say why, may be the world too much like mature adults too.")
+                .italic()
+                .frame(width:300, height:200, alignment: .top)
+                .padding(.top,-200)
+                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                .font(.subheadline)
+                .lineSpacing(5)
+                Text("我一直想跟別人分享他的故事，但始終未果，說不上為什麼，也許是這個世界太像大人太成熟。")
+                    .frame(width:300, height:200, alignment: .top)
+                    .padding(.top,-100)
+                    .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                    .font(.subheadline)
+                    .lineSpacing(5)
+                }
+
+            Image("03")
+                .resizable()
+                .frame(width: 320, height:300, alignment: .center)
+                .offset(x: 0, y: 100)
             }
+            .tabItem { Image(systemName:"sun.min") }
+            ZStack{
             RoundedRectangle(cornerRadius: 30)
                 .fill(Color(#colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)))
-                .padding()
+                .ignoresSafeArea()
+                VStack{
+            Text("I always think a person can be very naive simple live, countless people around will be used much more guarded.")
+                .italic()
+                .frame(width:300, height:200, alignment: .top)
+                .padding(.top,-200)
+                .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                .font(.subheadline)
+                .lineSpacing(5)
+                Text("我始終認為一個人可以很天真簡單的活下去，必是身邊無數人用更大的代價守護而來的。")
+                    .frame(width:300, height:200, alignment: .top)
+                    .padding(.top,-100)
+                    .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                    .font(.subheadline)
+                    .lineSpacing(5)
+                }
+
+            Image("04")
+                .resizable()
+                .frame(width: 320, height:300, alignment: .center)
+                .offset(x: 0, y: 100)
                 
+        }
+            .tabItem { Image(systemName:"sun.min") }
         }
         .tabViewStyle(PageTabViewStyle())
     }
---
+++ classic03/second .swift
@@ -0,0 +1,20 @@
+//
+//  second .swift
+//  classic03
+//
+//  Created by 暨大附中 on 2021/9/13.
+//
+
+import SwiftUI
+
+struct second_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct second__Previews: PreviewProvider {
+    static var previews: some View {
+        second_()
+    }
+}
